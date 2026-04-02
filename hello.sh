95f4a51794c8c0d79397b9e2a6ec7469f611d250
carlosvtsdev@gmail.com <carlosvtsdev@gmail.com>
2026-04-01 23:13:58 -0300

    declaring variables

--- hello.sh
+++ hello.sh
@@ -7,4 +7,10 @@ echo $PATH
 echo $PWD
 echo $USER
 echo $SHELL # what shell is running
-echo $HOSTNAME
+echo "$HOSTNAME"
+
+# Usually, use aspas in variables to echo exactly on how it is
+# creating a variable
+thing=`uname -a`
+# but you can also do 
+thing=$(uname -a) # cleaner!
